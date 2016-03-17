48845eae0b098d2c20baca0dc1ce9ff4303eb223
xikunlun001 <chenxi@ChenXis-MacBook-Air.local>
2016-03-16 23:43:02 -0500

    similarity

--- similarity.py
+++ similarity.py
@@ -14,3 +14,23 @@ def simDistance(w1,w2):
         sim += wi1*wi2
     sim = sim/(vectorlength(w1)*vectorlength(w2))
     return sim
+
+def computerSimilarity(query, listOfVectors):
+    indexList= []
+    if not listOfVectors:
+        print query + "did not match any documents."
+        return indexList
+
+    if listOfVectors:
+        for vector in listOfVectors:
+            value = simDistance(query,vector)
+            indexList.append(value)
+        indexList.sort()
+        return indexList
+"""
+query = [1,2,3,0]
+listOfVectors = [[1,0,0,0],[1,3,1,1]]
+
+output = computerSimilarity(query, listOfVectors)
+print output
+"""
