e493fd09736ff885c21dfbd4776c03eba105772d
xikunlun001 <chenxi860214@gmail.com>
2016-04-22 21:08:00 -0500

    Added files via upload

---
+++ candidateVector.py
@@ -0,0 +1,12 @@
+## by Xi Chen
+
+def extractCandidates(VectorList, Files_indice):
+    
+    return [VectorList[i] for i in Files_indice]
+    
+    
+""""    
+b = [4,2,3,4,1]
+c =[0,2]
+print extractCandidates(b,c)
+"""
